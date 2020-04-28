79c88f9bdf19ee1426735918bb256bc02e7a2de7
Aditya Sen <aloysen951@gmail.com>
2020-04-28 22:05:12 +0530

    addin the required changes for the server to accept multiple connections from 3rd party contacts

--- README.md
+++ README.md
@@ -1,10 +1,81 @@
-Details in the following YouTube videos ...
 
-Part 1
-https://youtu.be/h2WkZ0h0-Rc
 
-Part 2
-https://youtu.be/UMy6vV4tW00
+### `yarn start`
 
-Part3
-https://youtu.be/znw78jlFxqA
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+### `yarn test`
+
+Launches the test runner in the interactive watch mode.<br />
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `yarn build`
+
+Builds the app for production to the `build` folder.<br />
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.<br />
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
+### `yarn eject`
+
+**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+
+If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+
+Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+
+You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+
+## Learn More
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
+
+### Code Splitting
+
+This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
+
+### Analyzing the Bundle Size
+
+This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
+
+### Making a Progressive Web App
+
+This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
+
+### Advanced Configuration
+
+This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
+
+### Deployment
+
+This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
+
+### `yarn build` fails to minify
+
+This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+### what to do to deploy the system
+
+```shell
+yarn install : to install all the dependencies
+
+yarn serve : to start serving from the build folder as static files
+
+change the DOMAIN name in App.js
+yarn build to create the updated files in the build folder using react-scripts build tool
+
+```
+
+**Note: This has to be done else there shall be cors errors for the deployment
+
+In the App.js change up the version of the deployment to match the ngrok system
+this is basically utilizing test url to deploy for the time being
--- server.js
+++ server.js
@@ -4,43 +4,86 @@ const ngrok = require('ngrok');
 const user = process.env.USER;
 const password = process.env.PASSWORD;
 require('dotenv').config();
+//var cors = require('cors');
 
 var io = require('socket.io')
 ({
-  path: '/webrtc'
+  path: '/io/webrtc'
 })
 
 const app = express()
-const port = process.env.PORT || 8080 ;
+const port =process.env.PORT || 8080
 
+
+// use it before all route definitions
+//app.use(cors({origin: `http://localhost:${port}`}));
 // app.get('/', (req, res) => res.send('Hello World!!!!!'))
 
+
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', `http://localhost:${port}`);
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+const cors= function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', `http://localhost:${port}`);
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+}
 //https://expressjs.com/en/guide/writing-middleware.html
 app.use(express.static(__dirname + '/build'))
-app.get('/', (req, res, next) => {
+app.get('/',cors, (req, res, next) => {
     res.sendFile(__dirname + '/build/index.html')
 })
-
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
 ngrok.connect({
   proto : 'http',
   addr : process.env.PORT,
   //auth : `${user}:${password}`
-}, (err, url) => {
-  if (err) {
-      console.error('Error while connecting Ngrok',err);
-      return new Error('Ngrok Failed');
-  } else {
-      console.log('Tunnel Created -> ', url);
-      console.log('Tunnel Inspector ->  http://127.0.0.1:4040');
-  }
 }).then(uri =>{
-  console.log(uri);
+  console.log('Tunnel Created -> ', uri);
+  console.log('Tunnel Inspector ->  http://127.0.0.1:4040');
 }).catch(err=>{
-  console.error(err);
+  console.error('Error while connecting Ngrok', err);
   console.trace();
+  return new Error('Ngrok Failed');
 });
+
 io.listen(server)
+
+// default namespace
+io.on('connection', socket => {
+  console.log('connected')
+})
 
 // https://www.tutorialspoint.com/socket.io/socket.io_namespaces.htm
 const peers = io.of('/webrtcPeer')
@@ -50,34 +93,86 @@ let connectedPeers = new Map()
 
 peers.on('connection', socket => {
 
-  console.log(socket.id)
-  socket.emit('connection-success', { success: socket.id })
-
   connectedPeers.set(socket.id, socket)
+
+  console.log(socket.id)
+  socket.emit('connection-success', {
+    success: socket.id,
+    peerCount: connectedPeers.size,
+  })
+
+  const broadcast = () => socket.broadcast.emit('joined-peers', {
+    peerCount: connectedPeers.size,
+  })
+  broadcast()
+
+  const disconnectedPeer = (socketID) => socket.broadcast.emit('peer-disconnected', {
+    peerCount: connectedPeers.size,
+    socketID: socketID
+  })
 
   socket.on('disconnect', () => {
     console.log('disconnected')
     connectedPeers.delete(socket.id)
+    disconnectedPeer(socket.id)
   })
 
-  socket.on('offerOrAnswer', (data) => {
-    // send to the other peer(s) if any
-    for (const [socketID, socket] of connectedPeers.entries()) {
+  socket.on('onlinePeers', (data) => {
+    for (const [socketID, _socket] of connectedPeers.entries()) {
       // don't send to self
-      if (socketID !== data.socketID) {
-        console.log(socketID, data.payload.type)
-        socket.emit('offerOrAnswer', data.payload)
+      if (socketID !== data.socketID.local) {
+        console.log('online-peer', data.socketID, socketID)
+        socket.emit('online-peer', socketID)
       }
     }
   })
 
+  socket.on('offer', data => {
+    for (const [socketID, socket] of connectedPeers.entries()) {
+      // don't send to self
+      if (socketID === data.socketID.remote) {
+        // console.log('Offer', socketID, data.socketID, data.payload.type)
+        socket.emit('offer', {
+            sdp: data.payload,
+            socketID: data.socketID.local
+          }
+        )
+      }
+    }
+  })
+
+  socket.on('answer', (data) => {
+    for (const [socketID, socket] of connectedPeers.entries()) {
+      if (socketID === data.socketID.remote) {
+        console.log('Answer', socketID, data.socketID, data.payload.type)
+        socket.emit('answer', {
+            sdp: data.payload,
+            socketID: data.socketID.local
+          }
+        )
+      }
+    }
+  })
+
+  // socket.on('offerOrAnswer', (data) => {
+  //   // send to the other peer(s) if any
+  //   for (const [socketID, socket] of connectedPeers.entries()) {
+  //     // don't send to self
+  //     if (socketID !== data.socketID) {
+  //       console.log(socketID, data.payload.type)
+  //       socket.emit('offerOrAnswer', data.payload)
+  //     }
+  //   }
+  // })
+
   socket.on('candidate', (data) => {
     // send candidate to the other peer(s) if any
     for (const [socketID, socket] of connectedPeers.entries()) {
-      // don't send to self
-      if (socketID !== data.socketID) {
-        console.log(socketID, data.payload)
-        socket.emit('candidate', data.payload)
+      if (socketID === data.socketID.remote) {
+        socket.emit('candidate', {
+          candidate: data.payload,
+          socketID: data.socketID.local
+        })
       }
     }
   })
