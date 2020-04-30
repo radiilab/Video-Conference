b714a574940a154c75a86f6f5f00bf20e657b0d0
Aditya Sen <aloysen951@gmail.com>
2020-05-01 00:16:07 +0530

    remove cors from the dependency injection by the server

--- server.js
+++ server.js
@@ -40,27 +40,27 @@ app.use(function (req, res, next) {
   next();
 });
 
-const cors= function (req, res, next) {
+// const cors= function (req, res, next) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', `http://localhost:${port}`);
+//   // Website you wish to allow to connect
+//   res.setHeader('Access-Control-Allow-Origin', `http://localhost:${port}`);
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   // Request headers you wish to allow
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true);
 
-  // Pass to next layer of middleware
-  next();
-}
+//   // Pass to next layer of middleware
+//   next();
+// }
 //https://expressjs.com/en/guide/writing-middleware.html
 app.use(express.static(__dirname + '/build'))
-app.get('/',cors, (req, res, next) => {
+app.get('/', (req, res, next) => {
     res.sendFile(__dirname + '/build/index.html')
 })
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`))
